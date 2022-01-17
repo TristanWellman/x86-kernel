1509c38b7903809a2a36755d8420564a48f7ce92
__Oblivion__ <88514898+TristanWellman@users.noreply.github.com>
2022-01-17 15:27:23 -0500

    Delete build.sh

--- build.sh
+++
@@ -1,8 +0,0 @@
-cd src
-
-echo ":: compiling asm files"
-nasm -f elf32 start0.asm -o start.o
-echo ":: compiling c files"
-gcc -m32 -std=c11 -Wall -Wextra -Wstrict-aliasing -c kernel.c graphics.c GOL.c
-
-ld -m elf_i386 -T linker.ld -o kernel start.o kernel.o graphics.o GOL.o
