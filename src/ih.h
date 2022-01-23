573fce018a07792e2578bc8c43981ae204c8529a
__Oblivion__ <88514898+TristanWellman@users.noreply.github.com>
2022-01-23 12:35:47 -0800

    Add files via upload

---
+++ src/ih.h
@@ -0,0 +1,28 @@
+#undef __ASSEMBLY__
+
+#ifndef __ASSEMBLY__
+struct cpu_state {
+
+	unsigned int eax;
+	unsigned int ebx;
+	unsigned int ecx;
+
+	unsigned int esp;
+
+} __attrib__;
+
+struct stack_state {
+
+	unsigned int ec;
+
+	unsigned int eip;
+	unsigned int cs;
+	unsigned int eflags;
+
+}__attrib0__;
+
+#else
+
+void interrupt_handler(struct cpu_state cpu, struct stack_state stack, unsigned int interrupt);
+
+#endif
---
+++ src/keyboard.h
@@ -0,0 +1,42 @@
+#ifndef KEYBOARD_H
+#define KEYBOARD_H
+
+#include "u.h"
+
+/*first function and defs are for the interrupt controller or PIC*/
+
+#define PIX1 0x20
+#define PIX2 0xA0
+
+#define PIC1_SI 0x20
+#define PIC2_SI 0x28
+
+#define PIC2_EI PIC2_SI + 7
+
+#define PIC_ACK 0x20
+
+void pic_ack(unsigned int interrupt) {
+
+  if(interrupt < PIC1_SI || interrupt > PIC2_EI) {
+
+    return;
+
+  }
+
+  if(interrupt < PIC2_SI) {
+    outPB(PIX1, PIC_ACK);
+  } else {
+    outPB(PIX2, PIC_ACK);
+  }
+
+}
+
+#define KEYBOARD_DATAP 0x60
+
+unsigned char READ_SC(void) {
+
+  return inB(KEYBOARD_DATAP);
+
+}
+
+#endif
